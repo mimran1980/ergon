3b1eecb174df9b8b5246abc1e9ee656db579ed88
Mo Imran <mimran1980@gmail.com>
2026-08-02 19:09:56 +0800

    style: cargo fmt

--- sbe/tests/hft_008_checked_unchecked_test.rs
+++ sbe/tests/hft_008_checked_unchecked_test.rs
@@ -10,7 +10,13 @@
 //! 3. Injects an in-module measurement helper (so private cores are callable)
 //!    and records multi-scenario Instant samples for the keep matrix.
 
-#![allow(clippy::all, clippy::pedantic, clippy::restriction, clippy::nursery, unused_unsafe)]
+#![allow(
+    clippy::all,
+    clippy::pedantic,
+    clippy::restriction,
+    clippy::nursery,
+    unused_unsafe
+)]
 use std::error::Error;
 use std::time::Instant;
 
